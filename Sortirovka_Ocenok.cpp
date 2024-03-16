1ccabb148ddc1d5ea5f6ad6580b4a1c7adad5ffd
TiM3rk <146587349+TiM3rk@users.noreply.github.com>
2024-03-16 19:13:34 +0300

    Сортировка оценок
    У вас есть массив оценок студентов, в котором могут быть дубликаты. Вам нужно отсортировать этот массив таким образом, чтобы сначала шли уникальные оценки в порядке убывания, а затем - дубликаты в том же порядке, в котором они встречаются в исходном массиве. Например, если исходный массив оценок выглядит как 7, 2, 5, 7, 8, 2, 3, то отсортированный массив должен выглядеть как 8, 5, 3, 7, 7, 2, 2.

---
+++ Sortirovka_Ocenok.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+bool Unique(int* arr, int size, int element)
+{
+	int unique = 1;
+	for (int i = 0; i < size; i++)
+	{
+		if (element != i && arr[i] == arr[element])
+		{
+			return 0;;
+		}
+	}
+	return 1;
+}
+
+void SortGrades(int* grades, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i+1; j < size; j++)
+		{
+			if (grades[j] > grades[i] && Unique(grades, size, j))
+			{
+				int temp = grades[i];
+				grades[i] = grades[j];
+				grades[j] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n = 7;
+	int* prototype{ new int[n] {7, 2, 5, 7, 8, 2, 3 } };
+	SortGrades(prototype, n);
+	for (int i = 0; i < n; i++)
+	{
+		printf_s("%d ", prototype[i]);
+	}
+}
